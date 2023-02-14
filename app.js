6e359b24fe72a83574a1b40a3bee061bf07155ac
Jc007zZ <116668785+Jc007zZ@users.noreply.github.com>
2023-02-14 16:37:20 -0300

    press to mouse add

--- app.js
+++ app.js
@@ -1,28 +1,58 @@
 var color = 'black'
-
+let apertou
+let corEscolhida
 
 for (i = 1; i <= 289; i++) {
-    document.querySelector('main.quadro').innerHTML += ` <div class="cubo" id="${i}" onclick="pintar('${i}')"></div> `
+    document.querySelector('main.quadro').innerHTML += ` <div class="cubo" id="${i}" onmousedown="pintar(${i})" onmouseenter="PINTAR('${i}')" ></div> `
     console.log(i)
 }
+
+document.querySelector('.quadro').addEventListener('mousedown', function apertado() { apertou = true 
+    console.log(apertou)
+});
+
+document.querySelector('.quadro').addEventListener('mouseup', function apertado() { apertou = false 
+    console.log(apertou)
+});
+
 
 function cor(COR) {
     color = COR
     console.log(color)
     document.getElementById('cor').style.background = COR
-
+    
 }
-function pintar(id) {
 
-switch(color){
+function PINTAR(id) {
+    if ( apertou == true) {
+
+        switch(color){
+            case 'black':
+        document.getElementById(id).style.background = 'black'
+        break;
+        case 'white':
+            document.getElementById(id).style.background = 'white' 
+            break;
+    }
+}
+}
+
+
+function pintar(id) {
+    switch(color){
     case 'black':
     document.getElementById(id).style.background = 'black'
     break;
     case 'white':
         document.getElementById(id).style.background = 'white'
-        
+        break;
+}
 }
 
+function criarCor() {
+    corEscolhida = document.getElementById('criarCor').value
+    document.getElementById('cor').style.background = corEscolhida
+    document.querySelector
 }
 
 function limpar() {
